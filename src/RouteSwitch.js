ba879f31495aa4708c2fdaa969b098724b1dffc2
creme332 <65414576+creme332@users.noreply.github.com>
2023-05-27 21:31:21 +0400

    add mantine

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import HeaderWithTabs from "./components/HeaderWithTabs";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import FooterSocial from "./components/FooterSocial";
 
 const RouteSwitch = () => {
+  const tabs = [
+    { tabName: "Home", pathname: "/", id: "home-tab" },
+    { tabName: "Other", pathname: "/leaderboard", id: "other-tab" },
+  ];
+  const [colorScheme, setColorScheme] = useState("dark");
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <BrowserRouter
-    // basename="/subdirectory/cra-template/build" // comment this line if your project is NOT served from a sub-directory
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <BrowserRouter>
+          <HeaderWithTabs links={tabs} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <FooterSocial />
+        </BrowserRouter>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,11 @@
-import { Link } from "react-router-dom";
-import leaf from "./../assets/images/leaf.png";
-import "./../styles/Home.css";
+import { Container } from "@mantine/core";
+import HeroSection from "../components/HeroSection";
 
 function Home() {
   return (
-    <div className="Home">
-      <header>
-        <p>⚛ cra-template</p>
-        <img height={100} width={100} src={leaf} alt="A leaf" />
-        <Link to="/contact">
-          <p>Go to contact</p>
-        </Link>
-      </header>
-    </div>
+    <Container>
+      <HeroSection />
+    </Container>
   );
 }
 
